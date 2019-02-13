f3a714c5fde9ecfd97cc4a513c0b28074b093ec0
Chandler Cain <clcain@vt.edu>
2019-02-12 22:21:45 -0500

    Add index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const aws = require("aws-sdk");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello, world!");
+});
+
+app.post("/translate", (req, res) => {
+    let translator = new aws.Translate({ region: "us-east-1" });
+    translator.translateText({
+        "Text": req.body.Text,
+        "SourceLanguageCode": req.body.SourceLanguageCode,
+        "TargetLanguageCode": req.body.TargetLanguageCode
+    }, (error, data) => {
+        if (error) {
+            res.status(500).send({ error: error });
+        }
+        else {
+            res.status(200).send({ result: data.TranslatedText });
+        }
+    });
+});
+
+app.listen(8000, () => {
+    console.log("Application is running.");
+});
